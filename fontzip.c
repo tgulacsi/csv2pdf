5b83b6400c45d884ef3e966e735a65a59af99223
Tamás Gulácsi <tgulacsi78@gmail.com>
2014-04-30 20:28:08 +0200

    remove rsrc remnants

--- fontzip.c
+++
@@ -1,9 +0,0 @@
-#include "runtime.h"
-extern byte _brsrc_fontzip[], _ersrc_fontzip;
-
-/* func get_fontzip() []byte */
-void ·get_fontzip(Slice a) {
-  a.array = _brsrc_fontzip;
-  a.len = a.cap = &_ersrc_fontzip - _brsrc_fontzip;
-  FLUSH(&a);
-}
--- fontzip.go
+++
@@ -1,3 +0,0 @@
-package main
-
-func get_fontzip() []byte
